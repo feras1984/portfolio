072e3909eba306fec0da9660b3b1cf737667c885
Feras <87133014+feras1984@users.noreply.github.com>
2026-03-09 14:08:09 +0400

    update

--- resources/js/Services/FileService/FileService.ts
+++ resources/js/Services/FileService/FileService.ts
@@ -6,7 +6,7 @@ class FileService {
     DefaultImage = '/file/defaults/default-cover.jpg';
 
     static LOGO = "/file/logo";
-    static MAX_FILE_SIZE = 500; //500KB.
+    static MAX_FILE_SIZE = 5000; //5000KB.
     convertBase64 = (file: any) => {
         return new Promise<string|ArrayBuffer|null>((resolve, reject) => {
             const fileReader = new FileReader();
